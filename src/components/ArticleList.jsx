9fe32b6787b5117beb10984d8b4b872daab06383
Nick <nj.ratnarajah@gmail.com>
2025-05-22 09:56:44 +0100

    add 10-view-articles-by-topic

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,28 +1,25 @@
 import { useEffect, useState } from "react";
-import { requestArticles } from "../utils/axios";
 import ArticleCard from "./ArticleCard"
+import ArticleSearchBox from "./ArticleSearchBox";
 
 function ArticleList() {
     const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [topic, setTopic] = useState("")
 
-    useEffect(() => {
-        requestArticles().then((articles) => {
-            setArticles(articles);
-            setIsLoading(false);
-        });
-    }, []);
-
-    if (isLoading) return <p>Loading articles...</p>;
+ 
 
     return (
         <>
             <h1 className="articles-header">Articles</h1>
-            <ul className="article-list">
+            <ArticleSearchBox setTopic={setTopic} setArticles={setArticles} setIsLoading={setIsLoading} />
+            {isLoading ? (<p>Loading articles...</p>) :
+                (<ul className="article-list">
                 {articles.map((article) => (
                     <ArticleCard key={article.article_id} article={article} />
                 ))}
             </ul>
+        )}
         </>
     );
 }
--- src/components/ArticleSearchBox.jsx
+++ src/components/ArticleSearchBox.jsx
@@ -1,13 +1,36 @@
-function ArticleSearchBox() {
+import { useSearchParams } from "react-router-dom"
+import { useEffect } from "react"
+import { requestArticles } from "../utils/axios";
+
+
+function ArticleSearchBox({ setTopic, setArticles, setIsLoading }) {
+    const [searchParams] = useSearchParams()
+    const topicParam = searchParams.get("topic")
+    console.log(searchParams)
+
+    useEffect(() => {
+        setIsLoading(true)
+        setTopic(topicParam || "")
+        requestArticles(topicParam || "")
+          .then((articles) => {
+                setArticles(articles)
+                setIsLoading(false)
+            })
+            .catch(() => {
+                setArticles([])
+                setIsLoading(false)
+            })
+        }, [topicParam, setArticles, setIsLoading, setTopic])
+    
 
     return (
         <>
             <form className="article-search-box">
-                <label htmlFor="search-by-name">Search for Article</label>
+                {/* <label htmlFor="search-by-name">Search for Article</label>
                 <input type="text" id="search-by-name" name="search-by-name"/>
-                <button type="submit">Search</button>
-                <label htmlFor="filter-by-category">Filter by Category</label>
-                <input type="text" id="filter-by-category" name="filter-by-category"/>
+                <button type="submit">Search</button> */}
+                <label htmlFor="filter-by-topic">Filter by Topic</label>
+                <input type="text" id="filter-by-topic" name="topic"/>
                 <label htmlFor="sort-by">Sort By</label>
                 <select id="sort-by" name="sort-by" size="6">
                     <option>articleID</option>
@@ -23,6 +46,7 @@ function ArticleSearchBox() {
                     <option>ascending</option>
                     <option>descending</option>
                 </select>
+                <button>Go</button>
             </form>
         </>
     )
